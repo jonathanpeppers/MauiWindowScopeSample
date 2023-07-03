c2ac37c28a1df80da49874691c622671a04b90e2
Markus Rößler <markus.roessler@obi.de>
2023-07-03 11:06:47 +0200

    perform regular garbarge collection

--- MauiWindowScopeSample/App.xaml.cs
+++ MauiWindowScopeSample/App.xaml.cs
@@ -7,13 +7,21 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
+
+        Task.Run(async () =>
+        {
+            while (true)
+            {
+                GC.Collect(int.MaxValue, GCCollectionMode.Forced, true);
+                GC.WaitForPendingFinalizers();
+                await Task.Delay(2000);
+            }
+        });
 	}
 
     protected override Window CreateWindow(IActivationState activationState)
     {
         //typeof(MessagingCenter).GetMethod("ClearSubscribers", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, null);
-        GC.Collect(int.MaxValue, GCCollectionMode.Forced, true);
-        GC.WaitForPendingFinalizers();
         return activationState.Context.Services.GetRequiredService<MainWindow>();
     }
 }
