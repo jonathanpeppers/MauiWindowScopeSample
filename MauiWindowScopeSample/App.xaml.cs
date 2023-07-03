aad63d8a1f107452ee49f3f3bfd71a91e5df6daa
Markus Rößler <markus.roessler@obi.de>
2023-07-03 10:56:26 +0200

    added workarounds to resolve memory leak

--- MauiWindowScopeSample/App.xaml.cs
+++ MauiWindowScopeSample/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiWindowScopeSample;
+﻿using System.Reflection;
+
+namespace MauiWindowScopeSample;
 
 public partial class App : Application
 {
@@ -9,7 +11,9 @@ public partial class App : Application
 
     protected override Window CreateWindow(IActivationState activationState)
     {
+        //typeof(MessagingCenter).GetMethod("ClearSubscribers", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, null);
         GC.Collect(int.MaxValue, GCCollectionMode.Forced, true);
+        GC.WaitForPendingFinalizers();
         return activationState.Context.Services.GetRequiredService<MainWindow>();
     }
 }
